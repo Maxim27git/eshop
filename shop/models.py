4aa1d433f065d011e4031ea67f35152e4b697d92
Maxim27 <89885577000@mail.ru>
2025-08-12 18:32:25 +0300

    init

---
+++ shop/models.py
@@ -0,0 +1,25 @@
+
+from django.db import models
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.FloatField()
+    stock = models.IntegerField()
+    attributes = models.ManyToManyField('Attribute')
+
+
+
+
+class ProductImage(models.Model):
+    image = models.ImageField()
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+
+
+
+class Attribute(models.Model):
+    name = models.CharField(max_length=255)
+
+
+
