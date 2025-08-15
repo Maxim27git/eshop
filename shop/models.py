b12cd00a76bdeb587d871e22742b1cbe5b4a3038
Maxim27 <89885577000@mail.ru>
2025-08-15 19:43:56 +0300

    init

--- shop/models.py
+++ shop/models.py
@@ -1,12 +1,23 @@
 
 from django.db import models
+class ProductManager(models.Manager):
+    def in_stock(self):
+        return self.get_queryset().filter(stock__gt=0)
 
+    def get_sorted_by_price_desc(self):
+        """Возвращает товары, отсортированные по цене от большего к меньшему."""
+        return self.order_by("-price")
+
+    def get_sorted_by_price_asc(self):
+        """Возвращает товары, отсортированные по цене от меньшего к большему."""
+        return self.order_by("price")
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     price = models.FloatField()
     stock = models.IntegerField()
     attributes = models.ManyToManyField('Attribute')
+    objects = ProductManager()
 
 
 
